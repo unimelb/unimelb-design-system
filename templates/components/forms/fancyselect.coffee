2a5d956b74a57c6508040c394def42bac50724d1
Jason Smith <jason@waitingallday.com>
2015-01-30 11:50:23 +1100

    restyle select for moz, test in IE, adjust contrast on radio/checkbox border

--- templates/components/forms/fancyselect.coffee
+++ templates/components/forms/fancyselect.coffee
@@ -6,21 +6,15 @@ unless window.UOMFancySelect
 
         wrapper = document.createElement('div')
         wrapper.addClass('styled-select')
+        wrapper.addClass('alt') if @el.hasClass('alt')
+        wrapper.addClass('clear') if @el.hasClass('clear')
+        wrapper.addClass('clear-dark') if @el.hasClass('clear-dark')
         wrapper.innerHTML = """<svg class="icon" role="img"><use xlink:href="#icon-north-south"></use></svg>"""
 
         @el.parentNode.removeChild(@el)
-        wrapper.appendChild(@el)
         wrapper.insertBefore(@el, wrapper.firstChild)
 
         @parent.appendChild(wrapper)
 
     if (supportedmodernbrowser)
       new FancySelect(f) for f in document.querySelectorAll("select")
-
-  if window.attachEvent
-    window.attachEvent 'onload', ->
-      UOMFancySelect()
-  else
-    document.addEventListener 'DOMContentLoaded', ->
-      UOMFancySelect()
-
--- templates/uom.js
+++ templates/uom.js
@@ -3,22 +3,24 @@
 //= require_self
 
 window.UOMloadComponents = function() {
-  window.UOMAccordion()
-  window.UOMExtraLabel()
-  window.UOMformIcons();
-  window.UOMValid()
-  window.UOMListFilter()
-  window.UOMGMap()
-  window.UOMModal()
-  window.UOMTabs()
-  window.UOMInpageScrolling()
-  window.UOMStickyNav()
-  window.UOMUnlockChecklist()
-  window.UOMInnerNavTab()
-  window.UOMYtEmbed() // unused?
+  window.UOMAccordion();
+  window.UOMExtraLabel();
+  window.UOMFancySelect();
+  window.UOMValid();
+  window.UOMListFilter();
+  window.UOMGMap();
+  window.UOMModal();
+  window.UOMTabs();
+  window.UOMInpageScrolling();
+  window.UOMStickyNav();
+  window.UOMUnlockChecklist();
+  window.UOMInnerNavTab();
 };
 
-if (!window.attachEvent) {
+if (window.attachEvent) {
+  window.attachEvent('onload', window.UOMloadComponents);
+} else {
+  document.addEventListener('DOMContentLoaded', window.UOMloadComponents, false);
   document.addEventListener('page:change', function() {
     window.UOMloadComponents();
   }, false);
