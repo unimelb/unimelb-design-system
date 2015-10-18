2f76568933360475aa439d2ccea75faf21c44bb1
Jason Smith <jason@waitingallday.com>
2015-10-19 09:20:21 +1100

    switch to decodeURIComponent to cover other encoded chars

--- assets/targets/components/filtered-listings/index.js
+++ assets/targets/components/filtered-listings/index.js
@@ -161,7 +161,9 @@ ListFilter.prototype.filterCategories = function() {
       // Category matches
       var recs = category.getAttribute('data-category').split('|');
       for (var j=recs.length - 1; j >= 0; j--) {
-        if (recs[j].replace(' ','%20') == this.props.curr.replace(' ','%20'))
+        console.log(decodeURIComponent(recs[j]));
+        console.log(decodeURIComponent(this.props.curr));
+        if (decodeURIComponent(recs[j]) == decodeURIComponent(this.props.curr))
           showcategory = true;
       }
     }
