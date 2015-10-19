5c37956d6288896138a03be897f49c0dfd545255
Jason Smith <jason@waitingallday.com>
2015-10-19 16:07:21 +1100

    update to our own defined tileset id and accesstoken

--- assets/targets/components/maps/lmaps.js
+++ assets/targets/components/maps/lmaps.js
@@ -14,12 +14,11 @@ function LMaps(el, props) {
   this.props.map = L.map(this.el);
   this.props.map.setView(this.props.latlng, this.props.zoom);
 
-  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'examples.map-i875mjb7'
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'unimelb.cifxgw7yf40guudlyrvtisese',
+      accessToken: 'pk.eyJ1IjoidW5pbWVsYiIsImEiOiJjaWZ4Z3c5ZXo0M2R3dTdseGx0NXFyMmdiIn0.RIIkc7B1AboZclV3-JM5bA'
   }).addTo(this.props.map);
 
   if (this.el.getAttribute('data-pin')) {
