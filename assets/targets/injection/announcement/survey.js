38a6fee6d057198205070888c863c2e8ef119e2b
Jason Smith <jason@waitingallday.com>
2015-12-16 15:42:11 +1100

    try to integrate old injection announcement, fail

---
+++ assets/targets/injection/announcement/survey.js
@@ -0,0 +1,37 @@
+(function(root) {
+
+  if (document.cookie.replace(/(?:(?:^|.*;\s*)UOMsurveyTaken\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "tru") {
+
+    var body = document.body,
+        content = document.createElement('div');
+
+    content.setAttribute('class', 'page-announcement');
+    content.id = 'UOMannounce';
+    content.innerHTML = '<a class="page-announcement__message" target="_blank" href="https://www.surveymonkey.com/r/JP3VPSV"><h2>Do you have a minute?</h2><p>Help us improve our website by completing a short survey.</p></a><button id="UOMannounceClose" class="page-announcement__close" type="button">&times;</button>';
+
+    body.insertBefore(content, body.firstChild);
+    body.addClass('with-announcement');
+
+    var el = document.getElementById('UOMannounce');
+
+    // Hide on dismiss
+    document.getElementById('UOMannounceClose').addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.style.display = 'none';
+      document.body.removeClass('with-announcement');
+
+      // Dismiss forever, practically
+      document.cookie = "UOMsurveyTaken=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    });
+
+    // Hide on link click
+    el.addEventListener('click', function(e) {
+      this.style.display = 'none';
+      document.body.removeClass('with-announcement');
+
+      // Dismiss forever, practically
+      document.cookie = "UOMsurveyTaken=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    });
+  }
+
+})(this);
