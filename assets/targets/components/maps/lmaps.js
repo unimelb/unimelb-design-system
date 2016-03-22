a11bd0cb620028b1fdad5c534b50cc41fb3f0516
Jason Smith <jason@waitingallday.com>
2016-03-22 13:57:52 +1100

    fix reload problems on both mapping solutions

--- assets/targets/components/maps/lmaps.js
+++ assets/targets/components/maps/lmaps.js
@@ -12,21 +12,23 @@ function LMaps(el, props) {
   this.props.zoom = parseInt(this.el.getAttribute('data-zoom')) || 15;
   this.props.pins = this.el.getAttribute('data-pin');
 
-  this.props.map = L.map(this.el);
-  this.props.map.setView(this.props.latlng, this.props.zoom);
+  if (window.bound_lmaps[this.props.counter] !== undefined)
+    window.bound_lmaps[this.props.counter].remove();
+
+  window.bound_lmaps[this.props.counter] = L.map(this.el);
+  window.bound_lmaps[this.props.counter].setView(this.props.latlng, this.props.zoom);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'unimelb.cifxgw7yf40guudlyrvtisese',
       accessToken: 'pk.eyJ1IjoidW5pbWVsYiIsImEiOiJjaWZ4Z3c5ZXo0M2R3dTdseGx0NXFyMmdiIn0.RIIkc7B1AboZclV3-JM5bA'
-  }).addTo(this.props.map);
+  }).addTo(window.bound_lmaps[this.props.counter]);
 
-  
   if (this.props.pins) {
     var pins = this.props.pins.split('|');
     for (var i = pins.length - 1; i >= 0; i--) {
-      L.marker(pins[i].split(',')).addTo(this.props.map);
+      L.marker(pins[i].split(',')).addTo(window.bound_lmaps[this.props.counter]);
     }
   }
 }
