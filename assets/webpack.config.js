a1bbf524902736f05b7cf8ff9fe974277ef36fb4
Jason Smith <jason@waitingallday.com>
2016-09-13 13:41:44 +1000

    change autoprefixer version scheme to include minimum required browser variant, to maintain vendor prefixes as required

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -92,7 +92,7 @@ module.exports = {
   },
   postcss: [
     autoprefixer({
-      browsers: ['> 1% in AU', 'last 2 versions', 'Firefox ESR', 'ie >= 9']
+      browsers: ['> 1% in AU', 'last 2 versions', 'Firefox ESR', 'ie >= 9', 'iOS >= 8.4', 'Safari >= 8', 'Android >= 4.4']
     })
   ]
 };
