134ea3c4490039d4e436ed81740ae2c50a3157ed
Axel Bocciarelli <axelboc@gmail.com>
2016-11-09 10:57:03 +1100

    remove Array.prototype.slice polyfill

--- assets/shared/shims.js
+++ assets/shared/shims.js
@@ -47,41 +47,6 @@ require('classlist-polyfill');
     };
   }
 
-  // IE polyfill for Array.prototype.slice returning Object, source: MDN
-  var _slice = Array.prototype.slice;
-
-  try {
-    // Can't be used with DOM elements in IE < 9
-    _slice.call(document.documentElement);
-  }
-  catch (e) { // Fails in IE < 9
-    Array.prototype.slice = function (begin, end) {
-      var i, arrl = this.length, a = [];
-      // Although IE < 9 does not fail when applying Array.prototype.slice
-      // to strings, here we do have to duck-type to avoid failing
-      // with IE < 9's lack of support for string indexes
-      if (this.charAt) {
-        for (i = 0; i < arrl; i++) {
-          a.push(this.charAt(i));
-        }
-      }
-      // This will work for genuine arrays, array-like objects,
-      // NamedNodeMap (attributes, entities, notations),
-      // NodeList (e.g., getElementsByTagName), HTMLCollection (e.g., childNodes),
-      // and will not fail on other DOM objects (as do DOM elements in IE < 9)
-      else {
-        // IE < 9 (at least IE < 9 mode in IE 10) does not work with
-        // node.attributes (NamedNodeMap) without a dynamically checked length here
-        for (i = 0; i < this.length; i++) {
-          a.push(this[i]);
-        }
-      }
-      // IE < 9 gives errors here if end is allowed as undefined
-      // (as opposed to just missing) so we default ourselves
-      return _slice.call(a, begin, end || a.length);
-    };
-  }
-
   // Find the first element child of an element (`Node.firstElementChild()` is not supported on IE8)
   if (!Element.prototype.findFirstElementChild) {
     Element.prototype.findFirstElementChild = function () {
