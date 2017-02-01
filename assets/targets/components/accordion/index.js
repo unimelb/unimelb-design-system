4d6016fd73f64b5b1371ca6d8fc421de95f6bc17
Axel Bocciarelli <axelboc@gmail.com>
2017-02-01 16:26:10 +1100

    a bit of clean up to start with

--- assets/targets/components/accordion/index.js
+++ assets/targets/components/accordion/index.js
@@ -8,55 +8,22 @@ function Accordion(el, props) {
   this.el = el;
   this.props = props;
 
+  // Don't initialise twice
+  if (this.el.hasAttribute('data-bound')) return;
+  this.el.setAttribute('data-bound', 'true');
+
   this.props.container = this.el.parentNode;
   this.props.hidden = this.props.container.querySelector('.accordion__hidden');
 
-  this.setupCloseButton();
   this.el.setAttribute('tabindex', '0');
   this.props.hidden.setAttribute('tabindex', '-1'); // make panel focusable
 
-  // Event bindings
-  if (!this.el.hasAttribute('data-bound')) {
-    this.el.addEventListener('click', this.handleClick.bind(this));
-    window.addEventListener('keydown', this.clickWithEnter.bind(this));
+  this.setupCloseButton();
+  this.el.addEventListener('click', this.handleClick.bind(this));
 
-    this.el.setAttribute('data-bound', true);
-  }
+  // HACK: IE9 doesn't fire click event when pressing Enter
+  window.addEventListener('keydown', this.clickWithEnter.bind(this));
 }
-
-Accordion.prototype.handleClick = function(e) {
-  e.preventDefault();
-
-  // Determine overall container to check for single focus
-  var container = this.props.container.parentNode;
-
-  // Reasonable expectations, if the trigger is wrapped
-  if (container.nodeName == 'TR' || container.parentNode.nodeName == 'TR') {
-    while (container.nodeName != 'TABLE') {
-      if (container.parentNode)
-        container = container.parentNode;
-    }
-  }
-
-  if (container && container.getAttribute('data-single-focus') === '') {
-    for (var recs=container.querySelectorAll('.accordion__visible'), i=recs.length - 1; i >= 0; i--) {
-      if (recs[i] !== this.props.container) {
-        recs[i].classList.remove('accordion__visible');
-      }
-    }
-  }
-
-  this.props.container.classList.toggle('accordion__visible');
-
-  // Manage focus smartly to avoid having to remove the outline with CSS
-  if (this.props.container.classList.contains('accordion__visible')) {
-    // Focus panel on open
-    this.props.hidden.focus();
-  } else {
-    // Blur target on close
-    this.el.blur();
-  }
-};
 
 Accordion.prototype.setupCloseButton = function() {
   var close = this.props.container.querySelector('.accordion__close');
@@ -82,6 +49,41 @@ Accordion.prototype.setupCloseButton = function() {
   }
 };
 
+Accordion.prototype.handleClick = function(e) {
+  e.preventDefault();
+
+  // Determine overall container to check for single focus
+  var container = this.props.container.parentNode;
+
+  // Reasonable expectations, if the trigger is wrapped
+  if (container.nodeName == 'TR' || container.parentNode.nodeName == 'TR') {
+    while (container.nodeName != 'TABLE') {
+      if (container.parentNode)
+        container = container.parentNode;
+    }
+  }
+
+  if (container && container.hasAttribute('data-single-focus')) {
+    for (var recs=container.querySelectorAll('.accordion__visible'), i=recs.length - 1; i >= 0; i--) {
+      if (recs[i] !== this.props.container) {
+        recs[i].classList.remove('accordion__visible');
+      }
+    }
+  }
+
+  this.props.container.classList.toggle('accordion__visible');
+
+  // Manage focus smartly to avoid having to remove the outline with CSS
+  if (this.props.container.classList.contains('accordion__visible')) {
+    // Focus panel on open
+    this.props.hidden.focus();
+  } else {
+    // Blur target on close
+    this.el.blur();
+  }
+};
+
+// HACK: IE9 doesn't fire click event when pressing Enter
 Accordion.prototype.clickWithEnter = function(e) {
   var elem = document.activeElement;
   if (elem == this.el) {
