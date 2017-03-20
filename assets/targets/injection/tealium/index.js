fb741b5d4bfb2c92d1cedaea84a04496dba49603
Axel Bocciarelli <axelboc@gmail.com>
2017-03-20 11:36:33 +1100

    expose utag_data object to global scope

--- assets/targets/injection/tealium/index.js
+++ assets/targets/injection/tealium/index.js
@@ -1,13 +1,13 @@
-(function(root) {
-  var utag_data = {};
+(function () {
+  window.utag_data = window.utag_data || {};
 
   // Track design system version
   var uomScript = document.querySelector('script[src$="uom.js"]');
   if (uomScript) {
     var uomVersion = /\/v([0-9]+\.[0-9]+(?:\.[0-9]+)?)\/uom\.js$/.exec(uomScript.src);
-    utag_data.uom_version = uomVersion.length >= 2 ? uomVersion[1] : 'cannot-parse-version';
+    window.utag_data.uom_version = uomVersion.length >= 2 ? uomVersion[1] : 'cannot-parse-version';
   } else {
-    utag_data.uom_version = 'script-not-found';
+    window.utag_data.uom_version = 'script-not-found';
   }
 
   // Load Tealium
@@ -16,4 +16,4 @@
     b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;
     a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
   })();
-})(this);
+})();
